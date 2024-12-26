4f802e831453e91fe8495f6edbf54afe6dab781f
Avinoor Singh <avinoor196@gmail.com>
2024-12-26 19:39:20 +0530

    update

---
+++ Daily DSA practice/End Dec and January Daily Leetcode/35. Search Insert Portion.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int ans;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == target)
+            {
+                ans = i;
+                break;
+            }
+            else if (nums[i] > target)
+            {
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+};

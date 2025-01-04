14e5ff234c6b4e37b06714360c2a4a9032dcbd18
Avinoor Singh <avinoor196@gmail.com>
2025-01-04 08:52:33 +0530

    update.cpp

---
+++ Daily DSA practice/January DSA Leetcode Problems/153. Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+        int mini = INT_MAX;
+        int low = 0;
+        int n = nums.size();
+        int high = n - 1;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] <= nums[high])
+            {
+                if (nums[mid] <= mini)
+                {
+                    mini = nums[mid];
+                }
+                high = mid - 1;
+            }
+            else
+            {
+                if (nums[low] <= mini)
+                {
+                    mini = nums[mid];
+                }
+                low = mid + 1;
+            }
+        }
+        return mini;
+    }
+};

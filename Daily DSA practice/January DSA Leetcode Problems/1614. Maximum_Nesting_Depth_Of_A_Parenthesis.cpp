023743a59291f1020418830bf326a2d4b0d492b1
Avinoor Singh <avinoor196@gmail.com>
2025-01-26 15:27:16 +0530

    update

---
+++ Daily DSA practice/January DSA Leetcode Problems/1614. Maximum_Nesting_Depth_Of_A_Parenthesis.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int maxCount = -1;
+        int count = 0;
+        for(int i = 0;i<s.size();i++)
+        {
+            if(s[i] == '(')
+            {
+                count++;
+            }
+            else if(s[i]==')')
+            {
+                count--;
+            }
+            maxCount = max(maxCount,count);
+        }
+        return maxCount;
+    }
+};

a109367ddb105b62eff1326ceb63c7d3660e6983
Avinoor Singh <avinoor196@gmail.com>
2025-03-08 15:29:49 +0530

    update

--- Daily DSA practice/March Daily DSA Problems/combination2_sum.cpp
+++ Daily DSA practice/March Daily DSA Problems/combination2_sum.cpp
@@ -37,35 +37,3 @@ class Solution {
         }
     };
 =======
-class Solution
-{
-public:
-    void helper(vector<vector<int>> &data, vector<int> &nums, vector<int> &ans, int target, int i)
-    {
-        if (target == 0)
-        {
-            data.push_back(ans);
-            return;
-        }
-        if (i >= nums.size())
-        {
-            return;
-        }
-        if (nums[i] <= target)
-        {
-            ans.push_back(nums[i]);
-            helper(data, nums, ans, target - nums[i], i);
-            ans.pop_back();
-        }
-
-        helper(data, nums, ans, target, i + 1);
-    }
-    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
-    {
-        vector<vector<int>> data;
-        vector<int> ans;
-        helper(data, candidates, ans, target, 0);
-        return data;
-    }
-};
->>>>>>> be15416cc66d85f11523d196071ea5e0e670eb3c

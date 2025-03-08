0a529b82f232049b60198db80d4d23367646d020
Avinoor Singh <avinoor196@gmail.com>
2025-03-08 15:27:55 +0530

    update

---
+++ Daily DSA practice/March Daily DSA Problems/combination_sum.cpp
@@ -0,0 +1,37 @@
+
+class Solution {
+    public:
+        void helper(vector<int>&nums,int target,vector<int>&ans,int index,vector<vector<int>>&data)
+        {
+            if(target==0)
+            {
+                data.push_back(ans);
+                return;
+            }
+            if(index>=nums.size())
+            {
+                return;
+            }
+            int checker = INT_MIN;
+            for(int i = index;i<nums.size();i++)
+            {
+                if(nums[i]==checker) continue;
+                else
+                {
+                    checker = nums[i];
+                    if(nums[i]>target)break;
+                    ans.push_back(nums[i]);
+                    helper(nums,target-nums[i],ans,i+1,data);
+                    ans.pop_back();
+                }
+                
+            }
+        }
+        vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+            sort(candidates.begin(),candidates.end());
+            vector<vector<int>>data;
+            vector<int>ans;
+            helper(candidates,target,ans,0,data);
+            return data;
+        }
+    };

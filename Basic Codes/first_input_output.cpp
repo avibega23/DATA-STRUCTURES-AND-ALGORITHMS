90e9e4b8e40719409c3e739d1be8504c81dc95e3
Avinoor Singh <avinoor196@gmail.com>
2025-03-14 17:00:03 +0530

    update

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -3,25 +3,19 @@ using namespace std;
 
 int main()
 {
-	int row;
-	int col;
-	vector<vector<int>>mat(5,vector<int>(5));
-	for(int i = 0;i<=4;i++)
-	{
-		for(int j = 0;j<=4;j++)
-		{
-			cin >> mat[i][j];
-			if(mat[i][j]==1)
-			{
-				row = i+1;
-				col = j+1;
-			}
-		}
-	}
-	int calc1 = (row>3)?row-3:3-row;
+	int n, k, l, c, d, p, nl, np;
+	cin >> n;
+	cin >> k;
+	cin >> l;
+	cin >> c;
+	cin >> d;
+	cin >> p;
+	cin >> nl;
+	cin >> np;
 
-	int calc2 = (col>3)?col-3:3-col;
-	
-	cout << calc1+calc2;
-	return 0;
+	int res1 = ((k*l)/nl)/n;
+	int res2 = (c*d)/n;
+	int res3 = (p/np)/n;
+	int finalRes = min(res1,min(res2,res3));
+	cout << finalRes;
 }

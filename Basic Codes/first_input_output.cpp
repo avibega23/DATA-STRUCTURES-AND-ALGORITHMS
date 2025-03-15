2a1ee8a4d090908320a73ab27f39705797194895
Avinoor Singh <avinoor196@gmail.com>
2025-03-15 11:33:12 +0530

    update

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -3,19 +3,6 @@ using namespace std;
 
 int main()
 {
-	int n, k, l, c, d, p, nl, np;
-	cin >> n;
-	cin >> k;
-	cin >> l;
-	cin >> c;
-	cin >> d;
-	cin >> p;
-	cin >> nl;
-	cin >> np;
-
-	int res1 = ((k*l)/nl)/n;
-	int res2 = (c*d)/n;
-	int res3 = (p/np)/n;
-	int finalRes = min(res1,min(res2,res3));
-	cout << finalRes;
+	cout << 1-2+2*3 << endl;
+	return 0;
 }

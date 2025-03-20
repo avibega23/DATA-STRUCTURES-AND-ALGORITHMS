2d1b53051700ce513542713e1571eea13c767456
Avinoor Singh <avinoor196@gmail.com>
2025-03-20 06:09:31 +0530

    update

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -1,24 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int main()
 {
-	unordered_map<string,int> mpp = {
-		{"Tetrahedron",4},
-		{"Cube",6},
-		{"Octahedron",8},
-		{"Dodecahedron",12},
-		{"Icosahedron",20},
-	};
-	int testcase;
-	cin >> testcase;
-	int sum = 0;
-	for(int i = 0;i<testcase;i++)
-	{
-		string s ;
-		cin >> s;
-		sum+= mpp[s];
-	}
-	cout << sum << endl;
-
+	static int avi  = 1;
+	avi++;
+	cout << avi;
+	avi = 0;
+	cout << avi;
+	return 0;
+	
 }
---
+++ Daily DSA practice/March Daily DSA Problems/all_divisors_of_a_N_number.cpp
@@ -0,0 +1,10 @@
+void print_divisors(int n) {
+    // Code here.
+    for(int i = 1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            cout << i << " ";
+        }
+    }
+}

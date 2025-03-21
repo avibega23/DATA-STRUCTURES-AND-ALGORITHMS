987f4536be5a25b4d3707594004b6c863a56a8bc
Avinoor Singh <avinoor196@gmail.com>
2025-03-21 14:27:55 +0530

    update

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -1,14 +1,63 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Q{
+private:
+	int arr[4];
+	int currSize = 0;
+	int start = -1;
+	int end = -1;
+	int a = 4;
 
+public:
+
+	void push(int x)
+	{
+		if(currSize == 0)
+		{
+			start = end = 0;
+		}
+		else
+		{
+			end = (end+1)%a;
+		}
+		arr[end] = x;
+		
+		currSize++;
+	}
+	int pop()
+	{
+		if(currSize == 0) return -1;
+		int el = arr[start];
+		if(currSize == 1)
+		{
+			start = end = -1;
+		}
+		else
+		{
+			start = (start+1)%a;
+		}
+		currSize -= 1;
+		return el;
+	}
+	int top()
+	{
+		if(currSize == 0) return -1;
+		else
+		{
+			return arr[start];
+		}
+	}
+	int size()
+	{
+		return currSize;
+	}
+};
 int main()
 {
-	static int avi  = 1;
-	avi++;
-	cout << avi;
-	avi = 0;
-	cout << avi;
-	return 0;
-	
+	Q queue;
+	queue.push(5);
+	queue.push(6);
+	queue.push(9);
+	cout << queue.top();
 }

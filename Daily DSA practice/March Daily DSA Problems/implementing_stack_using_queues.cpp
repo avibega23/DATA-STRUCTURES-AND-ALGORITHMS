92501a7f6be1c26d1f40a4165c361bdb3b9b3097
Avinoor Singh <avinoor196@gmail.com>
2025-03-21 14:55:23 +0530

    update

--- Daily DSA practice/March Daily DSA Problems/implementing_stack_using_queues.cpp
+++ Daily DSA practice/March Daily DSA Problems/implementing_stack_using_queues.cpp
@@ -38,3 +38,64 @@ class MyStack {
      * int param_3 = obj->top();
      * bool param_4 = obj->empty();
      */
+
+
+
+
+     //optimal approach
+
+     class MyQueue {
+        public:
+            stack<int>st;
+            stack<int>st1;
+            MyQueue() {
+                st = {};
+                st1 = {};
+            }
+            
+            void push(int x) {
+                
+                    if(st.empty())
+                    {
+                        st.push(x);
+                        return;
+                    }
+                    else
+                    {
+                        while(!st.empty())
+                        {
+                            st1.push(st.top());
+                            st.pop();
+                        }
+                        st.push(x);
+                        while(!st1.empty())
+                        {
+                            st.push(st1.top());
+                            st1.pop();
+                        }
+                    }
+            }
+            
+            int pop() {
+                int top = st.top();
+                st.pop();
+                return top;
+            }
+            
+            int peek() {
+                return st.top();
+            }
+            
+            bool empty() {
+                return st.empty();
+            }
+        };
+        
+        /**
+         * Your MyQueue object will be instantiated and called as such:
+         * MyQueue* obj = new MyQueue();
+         * obj->push(x);
+         * int param_2 = obj->pop();
+         * int param_3 = obj->peek();
+         * bool param_4 = obj->empty();
+         */

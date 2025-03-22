8501191dc39762f539e2a4ba5a712349c83b2050
Avinoor Singh <avinoor196@gmail.com>
2025-03-23 01:10:07 +0530

    update

---
+++ Topic Wise/Bit Manipulation/stack and queues/Arithmetic Conversions/postfixToInfix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    public:
+      string postToInfix(string s) {
+          // Write your code here
+          int n = s.size();
+          int i = 0;
+          stack<string>st;
+          
+          while(i<n)
+          {
+              if((s[i]>='A' && s[i] <= 'Z')
+                  ||s[i] >= 'a' && s[i] <= 'z'
+                  ||s[i] >= '0' && s[i] <= '9'
+              )
+              {
+                  string str(1,s[i]);
+                  st.push(str);
+              }
+              else
+              {
+                  string t1 = st.top();
+                  st.pop();
+                  string t2 = st.top();
+                  st.pop();
+                  
+                  string curr = '('+t2 +s[i] + t1 + ')';
+                  st.push(curr);
+              }
+              i++;
+          }
+          return st.top();
+      }
+  };

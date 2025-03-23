e853c6e5c034c813614649c9297050c953970715
Avinoor Singh <avinoor196@gmail.com>
2025-03-23 18:23:30 +0530

    update

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -3,31 +3,8 @@ using namespace std;
 
 int main()
 {
-	vector<int> nums = {1,2,3,2,1,5,6,7};
-	stack<int>st;
-	int n = nums.size();
-	vector<int>ans(n,0);
-	for(int i = n-1;i>=0;i--)
-	{
-		while(!st.empty()&&nums[i]>=st.top())
-		{
-			st.pop();
-		}
-		if(st.empty())
-		{
-			
-			ans[i] = -1;
-		}
-		else
-		{
-			ans[i] = st.top();
-		}
-		st.push(nums[i]);
-	}
-
-	for(auto it:ans)
-	{
-		cout << it << " ";
-	}
-	return 0; 
+	int x = 3;
+	int y = x+1;
+	y++;
+	cout << x << " " << y;
 }

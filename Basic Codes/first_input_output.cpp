d78916aecb78448fa4d95735a5238302fd41dcbe
Avinoor Singh <avinoor196@gmail.com>
2025-04-12 20:19:18 +0530

    update

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -5,28 +5,8 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
-    cin>>n;
-    string ans = "YES";
-    if(n == 0)
-    {
-        ans = "NO";
-    }
-    int cnt = 0;
-    while(n!=0)
-    {
-        if(n%10!=7&&n%10!=4)
-        {
-            ans = "NO";
-        }
-        cnt++;
-        n/=10;
-    }
-    if(cnt == 4 || cnt == 7)
-    {
-        ans = "YES";
-    }
-    cout << ans;
+
     
+
     return 0;
 }

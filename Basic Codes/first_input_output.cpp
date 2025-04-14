f9948ecbba570a1626c061f28c1976ba529beafe
Avinoor Singh <avinoor196@gmail.com>
2025-04-14 13:16:48 +0530

    update

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -4,8 +4,22 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-
+    int n;
+    cin >> n;
+    cin.ignore();
+    int i = 0;
+    while(i<n)
+    {
+        string s;
+        getline(cin,s);
+        for(int i = 0;i<s.size();i++)
+        {
+            if(i == 0) cout << s[i];
+            if(s[i-1] == ' ') cout << s[i];
+        }
+        cout << endl;
+        i++;
+    }
     
 
     return 0;

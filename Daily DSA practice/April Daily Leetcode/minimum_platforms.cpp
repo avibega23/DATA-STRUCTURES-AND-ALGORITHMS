2c00dc1a05b7262a4a5a62ea30fa5170755a0830
Avinoor Singh <avinoor196@gmail.com>
2025-04-15 17:09:11 +0530

    update

---
+++ Daily DSA practice/April Daily Leetcode/minimum_platforms.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    int findPlatform(vector<int>& arr, vector<int>& dep) {
+        // Your code here
+        sort(arr.begin(),arr.end());
+        sort(dep.begin(),dep.end());
+        int i = 0;
+        int j = 0;
+        int cnt = 0;
+        int maxCnt = 0;
+        while(i<arr.size())
+        {
+            if(arr[i]<=dep[j])
+            {
+                cnt++;
+                i++;
+            }
+            else
+            {
+                cnt--;
+                j++;
+            }
+            maxCnt = max(cnt,maxCnt);
+        }
+        return maxCnt;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr, dep;
+        string input;
+
+        // Read first array (arr)
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        // Read second array (dep)
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            dep.push_back(number);
+        }
+
+        // Assuming Solution class is defined elsewhere with a method findPlatform
+        Solution ob;
+        cout << ob.findPlatform(arr, dep);
+
+        cout << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends

8350d61dfd9e99203a2e1499b3ea7f04fe80861a
Avinoor Singh <avinoor196@gmail.com>
2025-04-21 13:18:27 +0530

    update

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -5,27 +5,21 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    string s;
-    getline(cin,s);
-    vector<char>nums;
-    for(int i = 0;i<s.size();i++)
+    int n;
+    cin >> n;
+    while(n--)
     {
-        if(s[i]=='+')
-            continue;
-        else
+        int a,b,c;
+        int cnt = 0;
+        cin >> a >> b >> c;
+        while(a<=c)
         {
-            nums.push_back(s[i]);
+            b += a;
+            a += b;
+            cnt += 2;
         }
+        cout << cnt << endl;
     }
-    sort(nums.begin(),nums.end());
-    s = "";
-    for(int i = 0;i<nums.size();i++)
-    {
-        s += nums[i];
-        if(i !=nums.size()-1)
-        s += "+";
-    }
-    cout << s ;
 
     return 0;
 }

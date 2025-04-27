2d1957183793c53472b842a309a986bb833a62b3
Avinoor Singh <avinoor196@gmail.com>
2025-04-27 19:47:18 +0530

    update

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -1,44 +1,12 @@
-    #include <bits/stdc++.h>
-    using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-    int main() {
-        ios::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-        
-        int n;
-        cin >> n;
-        string s;
-        cin >> s;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // your code here
 
-        unordered_map<char,int>mpp;
-
-        for(auto it:s)
-        {
-            mpp[it]++;
-        }
-        for(auto it:mpp)
-        {
-            if(it.second % n != 0)
-            {
-              cout << -1 << endl;
-              return 0;  
-            } 
-        }
-        string ans = "";
-        for(auto it:mpp)
-        {
-            int num = it.second/n;
-            for(int i = 0;i<num;i++)
-            {
-                ans += it.first;
-            }
-        }
-        string newAns = "";
-        for(int i = 0;i<n;i++)
-        {
-            newAns += ans;
-        }
-        cout << newAns;
-        return 0;
-    }
+    return 0;
+}

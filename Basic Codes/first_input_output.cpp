faa8bfc783eff3406deb7d46e8ae5d235772d92b
Avinoor Singh <avinoor196@gmail.com>
2025-04-28 21:45:08 +0530

    updatet

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -1,64 +1,36 @@
-#include <bits/stdc++.h>
-using namespace std;
+    #include <bits/stdc++.h>
+    using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    
-    int n;
-    cin >> n;
+    int main() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        int t;
+        cin >> t;
+        
+        while(t--)
+        {
+            long long a,b,c;
+            cin >> a >> b >> c;
+           if(a < c)
+           {
+            cout << 1 << " ";
+           }
+           else
+           {
+            cout << -1 << " ";
+           }
+           if(b*a > c)
+           {
+            cout << b;
+           }
+           else
+           {
+            cout << -1;
+           }
+           cout << endl;
+        }
+        
 
-    for(int i = 0;i<=n;i++)
-    {
-        for(int j = 0;j<n-i;j++)
-        {
-            cout << "  ";
-        }
-        for(int j = 0;j<=i;j++)
-        {
-            cout << j;
-            if(i!=0)
-            cout << " ";
-        }
-        for(int j = i-1;j>=0;j--)
-        {
-            cout << j;
-            if(j!=0)
-            {
-                cout << " ";
-            }
-        }
-        cout << endl;
+        return 0;
     }
-
-    for(int i = n-1;i>=0;i--)
-    {
-        for(int j = 0;j<n-i;j++)
-        {
-            cout << "  ";
-            
-        }
-        for(int j = 0;j<=i;j++)
-        {
-            cout << j;
-            if(i != 0)
-            cout << " ";
-    
-        }
-        for(int j = i-1;j>=0;j--)
-        {
-            cout << j;
-            
-            if(j!=0)
-            {
-                cout << " ";
-            }
-        }
-        if(i!=0)
-        {
-            cout << endl;    
-        }
-    }
-    return 0;
-}
---
+++ CodeForces Problems/donut_shops.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        long long a,b,c;
+        cin >> a >> b >> c;
+       if(a < c)
+       {
+        cout << 1 << " ";
+       }
+       else
+       {
+        cout << -1 << " ";
+       }
+       if(b*a > c)
+       {
+        cout << b;
+       }
+       else
+       {
+        cout << -1;
+       }
+       cout << endl;
+    }
+    
+
+    return 0;
+}

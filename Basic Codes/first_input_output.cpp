7f4078806441a1222f992820be3b9822ecfc6edb
Avinoor Singh <avinoor196@gmail.com>
2025-04-29 22:14:07 +0530

    update

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -5,18 +5,22 @@
         ios::sync_with_stdio(false);
         cin.tie(0);
         cout.tie(0);
-        int t;
-        cin >> t;
-        
-       int n = 789658964;
-       int ans = 0;
-       while(n!=0)
-       {
-        int rem = n%10;
-        ans = (ans*10) + rem;
-        n /= 10;
-       }
-        cout << ans;
-
+        int n;
+        cin >> n;
+        int temp = n;
+        int cnt = 0;
+        for(int i = 1;i<=n;i++)
+        {
+            if(i == 1 || i == n)
+            {
+                cnt += temp;
+            }
+            else
+            {
+                cnt += ((temp-1)*i)+1;
+            }
+            temp--;
+        }
+        cout << cnt;        
         return 0;
     }
---
+++ CodeForces Problems/buttons.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    int temp = n;
+    int cnt = 0;
+    for(int i = 1;i<=n;i++)
+    {
+        if(i == 1 || i == n)
+        {
+            cnt += temp;
+        }
+        else
+        {
+            cnt += ((temp-1)*i)+1;
+        }
+        temp--;
+    }
+    cout << cnt;        
+    return 0;
+}

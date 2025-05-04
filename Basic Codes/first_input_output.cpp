92f0120a532f721fda67ec70d1f71096af4c7c67
Avinoor Singh <avinoor196@gmail.com>
2025-05-04 19:54:31 +0530

    update

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -6,38 +6,7 @@ int main() {
     cin.tie(0);
     cout.tie(0);
     
-    int n,first,second;
-    cin >> n >> first >> second;
-    vector<int>nums(n);
-    for(int i = 0;i<nums.size();i++)
-    {
-        cin >> nums[i];
-    }
-    for(int i = 0;i<nums.size();i++)
-    {
-        bool check = true;
-        for(int j = i-1;j>=0 && j>=i-first;j--)
-        {
-            if(nums[i]>=nums[j])
-            {
-                check = false;
-                break;
-            }
-        }
-        if(!check) continue;
-        for(int j = i+1;j<nums.size()&&j<=i+second;j++)
-        {
-            if(nums[i]>=nums[j])
-            {
-                check = false;
-                break;
-            }
-        }
-        if(check)
-        {
-            cout << i+1 << endl;
-            return 0;
-        } 
-    }
+    cout << 1 < endl;
+
     return 0;
 }

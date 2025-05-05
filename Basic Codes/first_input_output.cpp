69d691e2ac203731ab0f0da1e48f327021490ee8
Avinoor Singh <avinoor196@gmail.com>
2025-05-05 14:40:46 +0530

    update

--- Basic Codes/first_input_output.cpp
+++ Basic Codes/first_input_output.cpp
@@ -6,21 +6,7 @@ int main() {
     cin.tie(0);
     cout.tie(0);
     
-    int n;
-    string s;
-    cin >> n >> s;
+    cout << int get();
 
-    for(int i = 0;i<s.size()-1;i++)
-    {
-        if(s[i]!=s[i+1])
-        {
-            cout << "YES" << endl;
-            string ans = s.substr(i,2);
-            cout << ans << endl;
-            return 0;
-        }
-
-    }
-    cout << "NO";
     return 0;
-}
+}c

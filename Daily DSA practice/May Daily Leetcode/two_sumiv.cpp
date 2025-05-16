73427e1f18fa7a39306b54ecb1fdb9ea2d5660f7
Avinoor Singh <avinoor196@gmail.com>
2025-05-16 12:04:06 +0530

    update

--- Daily DSA practice/May Daily Leetcode/two_sumiv.cpp
+++ Daily DSA practice/May Daily Leetcode/two_sumiv.cpp
@@ -30,3 +30,83 @@ public:
 
 // optimal solution
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BstIterator{
+    public:
+        stack<TreeNode*>st;
+        bool reverse;
+        BstIterator(TreeNode* root,bool reverse)
+        {
+            this->reverse = reverse;
+            pushAll(root);
+        }
+        bool hasNext()
+        {
+            return !st.empty();
+        }
+        int next()
+        {
+            TreeNode* temp = st.top(); st.pop();
+            if(reverse == true)
+            {
+                TreeNode* node = temp->left;
+                for(; node!= NULL;)
+                {
+                    st.push(node);
+                    node = node->right;
+                }
+            }
+            else
+            {
+                TreeNode* node = temp->right;
+                for(; node!= NULL;)
+                {
+                    st.push(node);
+                    node = node->left;
+                }
+            }
+            return temp->val;
+        }
+        void pushAll(TreeNode* node)
+        {
+            for( ; node!= NULL ;)
+            {
+                st.push(node);
+                if(reverse)
+                {
+                    node = node->right;
+                }
+                else
+                {
+                    node = node->left;
+                }
+            }
+        }
+};
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        BstIterator a(root,false);
+        BstIterator b(root,true);
+
+        int i = a.next();
+        int j = b.next();
+        while(i<j)
+        {
+            if(i + j == k) return true;
+            else if(i+j < k) i = a.next();
+            else j = b.next();
+        }
+        return false;
+    }
+};

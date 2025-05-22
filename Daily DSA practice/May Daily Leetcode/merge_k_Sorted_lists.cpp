a492cff6bd30084125ad5922d3e95a3d5bdace57
Avinoor Singh <avinoor196@gmail.com>
2025-05-22 14:54:31 +0530

    update

--- Daily DSA practice/May Daily Leetcode/merge_k_Sorted_lists.cpp
+++ Daily DSA practice/May Daily Leetcode/merge_k_Sorted_lists.cpp
@@ -22,3 +22,53 @@ public:
         return list->next;
     }
 };
+
+
+//optimal solution 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    struct compare{
+        bool operator()(ListNode* a,ListNode* b){
+            return a->val>b->val;
+        }
+    };
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+        priority_queue<ListNode*,vector<ListNode*>,compare>pq;
+        for(int i = 0;i<lists.size();i++)
+        {
+            if(lists[i]!=NULL)
+            {
+                pq.push(lists[i]);
+            }
+        }
+        ListNode* list = new ListNode(-1);
+        ListNode* temp = list;
+
+        while(!pq.empty())
+        {
+            ListNode* linkList = pq.top();
+            pq.pop();
+
+            temp->next  = linkList;
+            temp = temp->next;
+
+            if(linkList->next != NULL)
+            {
+                pq.push(linkList->next);
+            }
+        }
+        return list->next;
+    }
+};

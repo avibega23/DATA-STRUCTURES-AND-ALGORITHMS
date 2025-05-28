92fbbd6ad3059f621a798860093dd45cb7833b06
Avinoor Singh <avinoor196@gmail.com>
2025-05-28 13:12:58 +0530

    update

---
+++ Daily DSA practice/May Daily Leetcode/bfs_of_a_graph.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // Code here
+        vector<int>vis(adj.size(),0);
+        vis[0] = 1;
+        queue<int>q;
+        q.push(0);
+        vector<int>ans;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adj[node])
+            {
+                if(vis[it]!=1)
+                {
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return ans;
+    }
+};

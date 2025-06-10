6febb2ba68f9fe3ee16e1c965aa006bd609babb9
Avinoor Singh <avinoor196@gmail.com>
2025-06-10 21:55:41 +0530

    update

--- Daily DSA practice/June Daily Leetcode Problems/djikstra_algo.cpp
+++ Daily DSA practice/June Daily Leetcode Problems/djikstra_algo.cpp
@@ -1,4 +1,5 @@
-// User Function Template
+
+// using pq // User Function Template
 class Solution {
   public:
     struct cmp{
@@ -35,6 +36,52 @@ class Solution {
                 {
                     dist[nv] = dist[v]+w;
                     pq.push({nv,dist[nv]});
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+
+
+//using sets
+
+
+// User Function Template
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code here
+        vector<vector<pair<int,int>>>adj(V);
+        for(int i = 0;i<edges.size();i++)
+        {
+            adj[edges[i][0]].push_back({edges[i][2],edges[i][1]});
+            adj[edges[i][1]].push_back({edges[i][2],edges[i][0]});
+        }
+        set<pair<int,int>>st;
+        vector<int>dist(V,INT_MAX);
+        dist[src] = 0;
+        st.insert({0,src});
+        
+        while(!st.empty())
+        {
+            auto it = st.begin();
+            int currDist = (*it).first;
+            int val = (*it).second;
+            st.erase(it);
+            
+            if(currDist > dist[val]) continue;
+            
+            for(auto it:adj[val])
+            {
+                int ndist = it.first;
+                int nval = it.second;
+                
+                if(dist[val]!=INT_MAX && dist[val]+ndist < dist[nval])
+                {
+                    dist[nval] = dist[val]+ndist;
+                    st.insert({dist[nval],nval});
                 }
             }
         }

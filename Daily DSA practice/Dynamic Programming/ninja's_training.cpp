ddf9301210c8a92e5aac2a3f19b485fde1a52f0c
Avi <avinoor196@gmail.com>
2025-06-29 14:01:09 +0530

    update

--- Daily DSA practice/Dynamic Programming/ninja's_training.cpp
+++ Daily DSA practice/Dynamic Programming/ninja's_training.cpp
@@ -29,3 +29,29 @@ int ninjaTraining(int n, vector<vector<int>> &points)
 
 
 //tabulation
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    // Write your code here.
+    vector<vector<int>>dp(n,vector<int>(4,-1));
+    dp[0][0] = max(points[0][1],points[0][2]);
+    dp[0][1] = max(points[0][0],points[0][2]);
+    dp[0][2] = max(points[0][1],points[0][0]);
+    dp[0][3] = max(points[0][1],max(points[0][0],points[0][2]));
+
+    for(int i = 1;i<n;i++)
+    {
+        for(int last = 0;last<4;last++)
+        {
+            for(int j = 0;j<3;j++)
+            {
+                if(j != last)
+                {
+                    int point = points[i][j] + dp[i-1][j];
+                    dp[i][last] = max(dp[i][last],point);
+                }
+            }
+        }
+    }
+
+    return dp[n-1][3];
+}

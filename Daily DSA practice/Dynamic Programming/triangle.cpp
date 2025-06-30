71316c6b62e9ffb1465e734eff1629df9daebc07
Avi <avinoor196@gmail.com>
2025-06-30 13:23:54 +0530

    update

--- Daily DSA practice/Dynamic Programming/triangle.cpp
+++ Daily DSA practice/Dynamic Programming/triangle.cpp
@@ -73,3 +73,29 @@ public:
 
 
 //space optimized solution
+class Solution {
+public:
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> dp(n);
+        for(int i = 0;i<n;i++)
+        {
+            dp[i] = triangle[n-1][i];
+        }
+
+        for(int i = n-2;i>=0;i--)
+        {
+            vector<int>temp(n);
+            for(int j = i;j >= 0;j--)
+            {
+                int down = triangle[i][j] + dp[j];
+                int right = triangle[i][j] + dp[j+1];
+
+                temp[j] = min(down,right);
+            }
+            dp = temp;
+        }
+        return dp[0];
+    }
+};

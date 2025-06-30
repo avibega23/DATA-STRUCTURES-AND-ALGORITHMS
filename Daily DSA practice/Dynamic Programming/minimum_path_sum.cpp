881f253d973c784863cc4435dae26a69c0e3687c
Avi <avinoor196@gmail.com>
2025-06-30 11:43:26 +0530

    update

--- Daily DSA practice/Dynamic Programming/minimum_path_sum.cpp
+++ Daily DSA practice/Dynamic Programming/minimum_path_sum.cpp
@@ -1,5 +1,7 @@
 //recursive solution TLE
 class Solution {
+
+
 public:
     int recurse(int i,int j,vector<vector<int>>&grid)
     {
@@ -18,3 +20,73 @@ public:
         return recurse(0,0,grid);
     }
 };
+
+//tabulated approach 
+class Solution {
+public:
+    
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        
+        for(int i = 0;i<m;i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
+                if(i == 0 && j == 0) dp[i][j] = grid[i][j];
+                else
+                {
+                    int up = grid[i][j];
+                    if(i>0)  up += dp[i-1][j];
+                    else up += 1e9;
+
+                    int left = grid[i][j];
+
+                    if(j>0) left += dp[i][j-1];
+                    else left += 1e9; 
+                    dp[i][j] = min(up,left);
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
+
+
+//tabulated + space optimized 
+class Solution {
+public:
+    
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int>dp(n,-1);
+        
+        for(int i = 0;i<m;i++)
+        {
+            vector<int>temp(n,-1);
+            for(int j = 0;j<n;j++)
+            {
+                if(i == 0 && j == 0) temp[j] = grid[i][j];
+                else
+                {
+                    int up = grid[i][j];
+                    if(i>0)  up += dp[j];
+                    else up += 1e9;
+
+                    int left = grid[i][j];
+
+                    if(j>0) left += temp[j-1];
+                    else left += 1e9; 
+                    temp[j] = min(up,left);
+                }
+            }
+            dp = temp;
+        }
+        return dp[n-1];
+    }
+};
+
+

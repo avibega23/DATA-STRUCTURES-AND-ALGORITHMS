9bb8848006f8186e35cc0152a0aac8fb98f79ebb
Avi <avinoor196@gmail.com>
2025-07-07 09:00:16 +0530

    update

--- Daily DSA practice/Dynamic Programming/Dp-on-subsequences/target_sum.cpp
+++ Daily DSA practice/Dynamic Programming/Dp-on-subsequences/target_sum.cpp
@@ -30,3 +30,41 @@ public:
 };
 
 //Tabulation
+//we are using the problem subset sum equals target into that
+class Solution {
+public:
+    int subsetSumToK(int n, int k, vector<int> &arr) {
+    // Write your code here.
+        vector<vector<int>>dp(n,vector<int>(k+1,0));
+
+        for(int i = 0;i<n;i++)
+        {
+            dp[i][0] = 1;
+        }
+        if (arr[0] >= 0 && arr[0] <= k)
+            dp[0][arr[0]] += 1;
+        for(int index = 1;index<n;index++)
+        {
+            for(int target = 0;target <= k;target++)
+            {
+                int nonPick = dp[index-1][target];
+                int pick = 0;
+                if(target>=arr[index])
+                pick = dp[index-1][target-arr[index]];
+
+                dp[index][target] = pick+nonPick;
+            }
+        }
+        return dp[n-1][k];
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int total = 0;
+        for(auto it:nums) total += it;
+
+        if((target + total)%2 != 0) return 0;
+        if(target+total < 0) return 0;
+        int s1 = (target+total)/2;
+
+        return subsetSumToK(nums.size(),s1,nums);
+    }
+};

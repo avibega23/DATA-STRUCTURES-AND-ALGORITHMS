773073536ab6c82ddaac82a625b5eadb9d2396a0
Avi <avinoor196@gmail.com>
2025-07-12 16:34:09 +0530

    update

---
+++ Daily DSA practice/Dynamic Programming/delete_operations_for_two_strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDistance(string text1, string text2) {
+        int ind1 = text1.size() , ind2 = text2.size();
+        vector<int>dp(text2.size()+1,0);
+        for (int i = 1; i <= ind1; i++) {
+            vector<int>temp(text2.size()+1,0);
+            for (int j = 1; j <= ind2; j++) {
+                if (text1[i - 1] == text2[j - 1])
+                    temp[j] = 1 + dp[j - 1];
+                else
+                    temp[j] = max(dp[j], temp[j - 1]);
+            }
+            dp = temp;
+        }
+        return (ind1+ind2)-(2*dp[ind2]);
+    }
+};

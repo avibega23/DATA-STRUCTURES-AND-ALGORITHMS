0206927d1640ec793ab3ee808c88d97ec973a052
Avi <avinoor196@gmail.com>
2025-07-12 16:06:18 +0530

    update

--- Daily DSA practice/Dynamic Programming/longest_palindromic_subsequence.cpp
+++ Daily DSA practice/Dynamic Programming/longest_palindromic_subsequence.cpp
@@ -19,4 +19,37 @@ public:
     }
 };
 
-//
+// using longest longestPalindromeSubsequence 
+
+class Solution {
+public: 
+    
+    int longestPalindromeSubseq(string s) {
+        string ans = s;
+        reverse(s.begin(),s.end());
+        int n = s.size();
+        int m = ans.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=m;j++)
+            {
+                if(s[i-1]==ans[j-1])
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else if(dp[i-1][j]>dp[i][j-1])
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};

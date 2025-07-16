79da6f6b8e2730d4546c4348a7312b9c8628e29d
Avi <avinoor196@gmail.com>
2025-07-16 09:38:51 +0530

    update

--- Daily DSA practice/Dynamic Programming/distinct_subsequences.cpp
+++ Daily DSA practice/Dynamic Programming/distinct_subsequences.cpp
@@ -18,4 +18,28 @@ public:
 };
 
 //tabulation Solution
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size(),m = t.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int j = 0;j<=n;j++)
+        {
+            dp[j][0] = 1;
+        }
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j] = (int)((long long)dp[i-1][j] + dp[i-1][j-1]);
+                }
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][m];
+    }
+};
+
 

42cae0196490d8762f6b8cf069aee13e3c724d6d
Avi <avinoor196@gmail.com>
2025-07-17 17:21:49 +0530

    update

--- Daily DSA practice/Dynamic Programming/wildcard_matching.cpp
+++ Daily DSA practice/Dynamic Programming/wildcard_matching.cpp
@@ -36,4 +36,27 @@ public:
 };
 
 //tabulation approach
-//
+class Solution {
+public:
+    bool isMatch(string s, string t) {
+        int n = s.size(), m = t.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        dp[0][0] = 1;
+         for (int j = 1; j <= m; ++j) {
+            if (t[j - 1] == '*')
+                dp[0][j] = dp[0][j - 1];
+        }
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=m;j++)
+            {
+               if (t[j - 1] == s[i - 1] || t[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (t[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};

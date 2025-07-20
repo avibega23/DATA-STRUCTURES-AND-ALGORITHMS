a458bc07339ab352cfd6a18f2295911d4bd1acb9
Avi <avinoor196@gmail.com>
2025-07-20 15:00:20 +0530

    update

--- Daily DSA practice/Dynamic Programming/Dp-on-Stocks/best_time_to_buy_and_sell_a_stock_2.cpp
+++ Daily DSA practice/Dynamic Programming/Dp-on-Stocks/best_time_to_buy_and_sell_a_stock_2.cpp
@@ -19,3 +19,26 @@ public:
         return recurse(prices,0,0,dp);
     }
 };
+
+//TABULATED
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        for(int i = n-1;i>=0;i--)
+        {
+            for(int j = 0;j<2;j++)
+            {
+                if(j == 1)
+                dp[i][j] = max(dp[i+1][1],-prices[i]+dp[i+1][0]);
+                else
+                dp[i][j] = max(dp[i+1][0],prices[i]+dp[i+1][1]);
+            }
+        }
+        return dp[0][1];
+    }
+};

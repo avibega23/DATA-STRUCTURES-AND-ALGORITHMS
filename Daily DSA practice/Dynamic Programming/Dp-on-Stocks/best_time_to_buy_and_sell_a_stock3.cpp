ac1e47b3aaf2b4ff1d3c193156de555285ded5cd
Avi <avinoor196@gmail.com>
2025-07-22 17:53:07 +0530

    update

--- Daily DSA practice/Dynamic Programming/Dp-on-Stocks/best_time_to_buy_and_sell_a_stock3.cpp
+++ Daily DSA practice/Dynamic Programming/Dp-on-Stocks/best_time_to_buy_and_sell_a_stock3.cpp
@@ -33,28 +33,6 @@ public:
 //tabulation
 class Solution {
 public:
-    int recurse(vector<int>&prices,int ind,bool buy,int cnt,vector<vector<vector<int>>>&dp)
-    {
-
-        int n = prices.size();
-        if(cnt == 0) return 0;
-        if(ind == n) return 0;
-
-        if(dp[ind][buy][cnt]!=-1) return dp[ind][buy][cnt];
-
-        long profit;
-
-        if(buy)
-        {
-            profit = max(recurse(prices,ind+1,buy,cnt,dp),prices[ind]+recurse(prices,ind+1,!buy,cnt-1,dp));
-        }
-        else
-        {
-            profit = max(recurse(prices,ind+1,buy,cnt,dp),-prices[ind]+recurse(prices,ind+1,!buy,cnt,dp));
-        }
-
-        return dp[ind][buy][cnt] = profit;
-    }
     int maxProfit(vector<int>& prices) {
          vector<vector<vector<int>>> dp(prices.size()+1, vector<vector<int>>(2, vector<int>(3, 0)));
         int n = prices.size();

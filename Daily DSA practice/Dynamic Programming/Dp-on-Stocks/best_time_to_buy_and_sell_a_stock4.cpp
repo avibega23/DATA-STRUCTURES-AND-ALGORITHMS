6e2375668970e4902f45b66d9fa2617955a0863b
Avi <avinoor196@gmail.com>
2025-07-22 18:51:54 +0530

    update

--- Daily DSA practice/Dynamic Programming/Dp-on-Stocks/best_time_to_buy_and_sell_a_stock4.cpp
+++ Daily DSA practice/Dynamic Programming/Dp-on-Stocks/best_time_to_buy_and_sell_a_stock4.cpp
@@ -3,22 +3,6 @@
 class Solution {
 public:
     int maxProfit(int x, vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size()+1, vector<vector<int>>(2, vector<int>(x+1, 0)));
-        int n = prices.size();
-        for(int i = n-1;i>=0;i--)
-        {
-            for(int j = 0;j<2;j++)
-            {
-                for(int k = 1;k<=x;k++)
-                {
-                    if(j == 1)
-                    dp[i][j][k] = max(dp[i+1][j][k],prices[i] + dp[i+1][0][k-1]);
-                    else
-                    dp[i][j][k] = max(dp[i+1][j][k],-prices[i] + dp[i+1][1][k]);
-                }
-            }
-        }
-
-        return dp[0][0][x];
+        
     }
 };

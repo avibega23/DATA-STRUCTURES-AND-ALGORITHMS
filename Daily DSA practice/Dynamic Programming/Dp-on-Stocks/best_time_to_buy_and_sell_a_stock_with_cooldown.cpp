83793ba2d4c15cec22f2270d08d075835cff25f6
Avi <avinoor196@gmail.com>
2025-07-22 18:23:31 +0530

    update

--- Daily DSA practice/Dynamic Programming/Dp-on-Stocks/best_time_to_buy_and_sell_a_stock_with_cooldown.cpp
+++ Daily DSA practice/Dynamic Programming/Dp-on-Stocks/best_time_to_buy_and_sell_a_stock_with_cooldown.cpp
@@ -1,15 +1,16 @@
+//recursion + memoization
 class Solution {
 public:
     int recurse(vector<int>&prices,int ind,int buy,vector<vector<int>>&dp)
     {
         int n = prices.size();
-        if(ind == n) return 0;
+        if(ind >= n) return 0;
         if(dp[ind][buy]!=-1)return dp[ind][buy];
 
         long profit = 0;
 
         if(buy == 0) profit = max(0+recurse(prices,ind+1,0,dp),-prices[ind]+recurse(prices,ind+1,1,dp));
-        else profit = max(0+recurse(prices,ind+1,1,dp),prices[ind]+recurse(prices,ind+1,0,dp));
+        else profit = max(0+recurse(prices,ind+1,1,dp),prices[ind]+recurse(prices,ind+2,0,dp));
 
         return dp[ind][buy] = profit;
     }
@@ -20,25 +21,27 @@ public:
     }
 };
 
-//TABULATED
+//tabulation
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-            int n = prices.size();
-            vector<vector<int>>dp(n+1,vector<int>(2,-1));
-            dp[n][0] = 0;
-            dp[n][1] = 0;
-            for(int i = n-1;i>=0;i--)
+        int n = prices.size();
+        vector<vector<int>>dp(n+2,vector<int>(2,0));
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        for(int i = n-1;i>=0;i--)
+        {
+            for(int j = 0;j<2;j++)
             {
-                for(int j = 0;j<2;j++)
-                {
-                    if(j == 1)
-                    dp[i][j] = max(dp[i+1][1],-prices[i]+dp[i+1][0]);
-                    else
-                    dp[i][j] = max(dp[i+1][0],prices[i]+dp[i+1][1]);
-                }
+                if(j == 0)
+                dp[i][j] = max(dp[i+1][0],-prices[i]+dp[i+1][1]);
+                else
+                dp[i][j] = max(dp[i+1][1],prices[i]+dp[i+2][0]);
             }
-            return dp[0][1];
+        }
+        return dp[0][0];
     }
 };
+
+

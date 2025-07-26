d8d49172a50a7e0462cde9427395d0d3ca67ebcd
Avi <avinoor196@gmail.com>
2025-07-26 18:51:31 +0530

    update

--- Daily DSA practice/Dynamic Programming/Dp-on-Partition/matrix_chain_multiplication.cpp
+++ Daily DSA practice/Dynamic Programming/Dp-on-Partition/matrix_chain_multiplication.cpp
@@ -23,4 +23,32 @@ class Solution {
 };
 
 //tabulation
+class Solution {
+  public:
 
+    int matrixMultiplication(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        int dp[n][n];
+        for(int i = 0;i<n;i++)
+        {
+            dp[i][i] = 0;
+        }
+        for(int i = n-1;i>=1;i--)
+        {
+            for(int j = i+1;j<n;j++)
+            {
+                int mini = INT_MAX;
+                for(int k = i;k<j;k++)
+                {
+                    int steps = (arr[i-1] * arr[k] * arr[j] ) +  dp[i][k] + dp[k+1][j] ;
+                    mini = min(mini,steps);
+                }
+                dp[i][j] = mini; 
+            }
+        }
+        
+        
+        return dp[1][n-1];
+    }
+};

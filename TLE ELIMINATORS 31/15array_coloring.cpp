fd2d223300b33d7be33ddc630994bf487bdbe9dc
Avinoor Singh <avinoor196@gmail.com>
2025-08-05 19:32:33 +0530

    update

---
+++ TLE ELIMINATORS 31/15array_coloring.cpp
@@ -0,0 +1,36 @@
+//https://codeforces.com/problemset/problem/1857/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define ff first
+#define ss second
+
+void solve() {
+    // Write code for one testcase here
+    int n;
+    cin >> n;
+    int sum = 0;
+    for(int i = 0;i<n;i++)
+    {
+        int num;
+        cin >> num;
+        sum += num;
+    }
+    if(sum%2==0) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;   // Comment if only 1 test case
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

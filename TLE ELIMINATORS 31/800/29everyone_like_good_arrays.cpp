982794b0f892a47e384d2dc2a7604fed5bd9c0c4
Avinoor Singh <avinoor196@gmail.com>
2025-08-18 20:02:09 +0530

    update

---
+++ TLE ELIMINATORS 31/800/29everyone_like_good_arrays.cpp
@@ -0,0 +1,42 @@
+//https://codeforces.com/problemset/problem/1777/A
+
+    #include <bits/stdc++.h>
+    using namespace std;
+
+    #define int long long
+    #define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+    #define all(x) (x).begin(), (x).end()
+    #define pb push_back
+    #define ff first
+    #define ss second
+
+    void solve() {
+        // Write code for one testcase here
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++) 
+        {
+            cin >> nums[i];
+        }
+        int cnt = 0;
+        for(int i = 0;i<n-1;i++)
+        {  
+            if(nums[i]%2 == nums[i+1]%2)
+            {
+                cnt++;
+                // nums[i+1] = nums[i]*nums[i+1];
+            }
+        }
+        cout << cnt << endl;
+    }
+
+    int32_t main() {
+        fast_io;
+        int t = 1;
+        cin >> t;   // Comment if only 1 test case
+        while (t--) {
+            solve();
+        }
+        return 0;
+    }
